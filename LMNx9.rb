22eb01c61cb28e8f51c55ee2d63b184430adb6b7
DARK_TEAM_LMNx9 <145145650+LMNx9-JOHNY@users.noreply.github.com>
2024-11-02 18:11:00 +0600

    Update LMNx9.rb

--- LMNx9.rb
+++ LMNx9.rb
@@ -1,20 +1,3 @@
 # Fuxk You Kids || - LMNx9
-
-require 'rbconfig'
-require 'open-uri'
-require_relative 'LMNx9'
-arch = RbConfig::CONFIG['host_cpu']
-if arch.include?('x86_64')
-  require_relative 'LMNx9'
-elsif arch.include?('i386') || arch.include?('i686')
-  system('clear')
-  system('xdg-open https://t.me/TEAM_LMNx9')
-  sleep(3)
-  puts "</> Your Device Is Not Supported For Run This Tool..!"
-  exit
-else
-  system('clear')
-  require_relative 'LMNx9'
-  puts "</> Something Went Wrong..! Try Again"
-  exit
-end
+LMNx9 = `LMNx9`
+puts LMNx9
